381f31e378396e72d050c3c12d8465d5d221b512
enderpyy <widderender@gmail.com>
2026-04-15 08:48:08 -0400

    fixed a zooming issue

--- machina/scripts/sensor_calibration.gd
+++ machina/scripts/sensor_calibration.gd
@@ -1,6 +1,6 @@
 class_name SensorCalibration extends Area2D
 
-var screen_size : Vector2
+
 
 var target_scene = load("res://scenes/target.tscn")
 
@@ -12,30 +12,41 @@ var background : Sprite2D = $Background
 var spawn : CollisionShape2D = $"Spawn Area"
 var spawn_size
 
-var initial_scale : Vector2
-var initial_position : Vector2
+var initial_scale : Vector2 #updated before zoom in
+var initial_position : Vector2 #updated before zoom in
 
 func _ready() -> void:
-	screen_size = background.texture.get_size() * background.scale
 	spawn_size = spawn.shape.get_rect().size
-	
-	zoom_in()
-
-func zoom(target_position : Vector2, target_scale : Vector2, speed : float = 0.05):
 	initial_scale = self.scale
 	initial_position = self.position
-	
-	while abs(position - target_position) > Vector2(5, 5) or abs(scale - target_scale) > Vector2(0.05, 0.05):
+	zoom_in()
+	await get_tree().create_timer(1).timeout
+	print("zooming out to " + str(initial_scale))
+	zoom_out()
+
+var zoom_id = 0
+
+#zoom to target position, at target scale, with lerp value
+func zoom(target_position : Vector2, target_scale : Vector2, speed : float = 0.05):
+	zoom_id += 1
+	var id = zoom_id
+	while abs(position - target_position) > Vector2(0.1, 0.1) or abs(scale - target_scale) > Vector2(0.01, 0.01):
+		if id != zoom_id:
+			return
+		
 		self.position = lerp(position, target_position, speed)
 		self.scale = lerp(scale, target_scale, speed)
 		await get_tree().process_frame
 
 
 func zoom_in():
+	initial_scale = self.scale
+	
+	initial_position = self.position
 	zoom(Vector2(960, 540), Vector2(1,1))
 
 func zoom_out():
-	pass
+	zoom(initial_position, initial_scale)
 
 func play( n := 5):
 	var score := 0.0
