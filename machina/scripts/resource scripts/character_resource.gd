7056b9ad38153537d44f1a32c85761acfb7080da
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2026-04-18 18:07:03 -0400

    character bug fix

--- machina/scripts/resource scripts/character_resource.gd
+++ machina/scripts/resource scripts/character_resource.gd
@@ -10,39 +10,35 @@ class_name CharacterResource extends Resource
 
 @export
 var character_name : String
-func set_character_name(n : String):
-	character_name = n
-func get_character_name() -> String:
-	return character_name
+#func set_character_name(n : String):
+	#character_name = n
+#func get_character_name() -> String:
+	#return character_name
 
 @export
-var sprite : Texture2D
-func set_sprite(s : Texture2D):
-	sprite = s
-func get_sprite() -> Texture2D:
-	return sprite
+var texture : Texture2D
+#func set_sprite(s : Texture2D):
+	#sprite = s
+#func get_sprite() -> Texture2D:
+	#return sprite
+@export
+var calibrator_transform : Transform2D
 
-
-var calibrator_position : Transform2D
-func set_calibrator_position(pos : Transform2D):
-	calibrator_position = pos
-func get_calibrator_position() -> Transform2D:
-	return calibrator_position
-
-var nut_positions : Array[Transform2D]
-func set_nut_positions(pos : Array[Transform2D]):
-	nut_positions = pos
-func get_nut_positions() -> Array[Transform2D]:
-	return nut_positions
-
-var oil_position : Transform2D
-func set_oil_position(pos : Transform2D):
-	oil_position = pos
-func get_oil_position() -> Transform2D:
-	return oil_position
-
-var charger_position : Transform2D
-func set_charger_position(pos : Transform2D):
-	charger_position = pos
-func get_charger_position() -> Transform2D:
-	return charger_position
+@export
+var nut_positions: Array[Transform2D]
+#func set_nut_positions(pos : Array[Transform2D]):
+	#nut_positions = pos
+#func get_nut_positions() -> Array[Transform2D]:
+	#return nut_positions
+@export
+var oil_position: Transform2D
+#func set_oil_position(pos : Transform2D):
+	#oil_position = pos
+#func get_oil_position() -> Transform2D:
+	#return oil_position
+@export
+var charger_position: Transform2D
+#func set_charger_position(pos : Transform2D):
+	#charger_position = pos
+#func get_charger_position() -> Transform2D:
+	#return charger_position
