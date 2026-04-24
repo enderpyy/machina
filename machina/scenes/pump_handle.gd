4bd89cbc36fe0a5fa716db176fcd494873c6b404
enderpyy <widderender@gmail.com>
2026-04-23 23:01:22 -0400

    pushings mth

--- machina/scenes/pump_handle.gd
+++ machina/scenes/pump_handle.gd
@@ -3,11 +3,14 @@ class_name Handle extends Area2D
 @export var minimum_height : int
 @export var maximum_height : int
 
+@onready var audio = $AudioStreamPlayer2D
+var pump = preload("res://audio/sfx/charger/pump.wav")
 
 signal down
 
 var previous_position
 var following := false
+var going_down := false
 func _physics_process(delta: float) -> void:
 	var mp = get_parent().get_local_mouse_position()
 	if following:
@@ -17,8 +20,15 @@ func _physics_process(delta: float) -> void:
 			self.position.y = maximum_height
 		elif self.position.y < minimum_height:
 			self.position.y = minimum_height
-		if previous_position.y < self.position.y:
+		if previous_position.y < self.position.y and going_down == false:
 			down.emit((self.position.y - previous_position.y)/(maximum_height-minimum_height))
+			going_down = true
+			audio.play()
+		elif previous_position.y <= self.position.y and going_down == true:
+			down.emit((self.position.y - previous_position.y)/(maximum_height-minimum_height))
+		else:
+			if going_down == true:
+				going_down = false
 	else:
 		pass # if charge 
 
